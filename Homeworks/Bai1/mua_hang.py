c0a6f4490552593d29beff5ae55c9c02d39a3f1b
LMinh <lminhtm@gmail.com>
2022-10-16 00:31:36 +0700

    [Homework] Bai1

---
+++ Homeworks/Bai1/mua_hang.py
@@ -0,0 +1,7 @@
+ten_hang = 'Sữa hộp Vinamilk'
+so_luong = 5
+don_gia = 25000
+print('Tên hàng:', ten_hang)
+print('Số lượng:', so_luong)
+print('Đơn giá:', don_gia)
+print('Tiền phải trả: %i vnđ' %(so_luong * don_gia))
---
+++ Homeworks/Bai1/tinh_toan_don_gian.py
@@ -0,0 +1,5 @@
+x, y = 10, 5
+print('Tong x + y =', x + y)
+print('Tong x - y =', x - y)
+print('Tong x * y =', x * y)
+print('Tong x / y =', x / y)
